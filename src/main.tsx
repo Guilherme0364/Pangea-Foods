8174bfae754de42a97f5999f66699f05247dfea7
Guilherme0364 <guilhermedg364@outlook.com>
2024-10-18 12:08:53 -0300

    feat: add routes possibilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx'
+import Products from './pages/Products.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/products" element={<Products />} />
+		</Routes>
+	</BrowserRouter>
+);
