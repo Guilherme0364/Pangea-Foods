768b4581f288f61883d48ab15f7f41fbfa103dd4
Guilherme0364 <guilhermedg364@outlook.com>
2024-10-18 12:36:40 -0300

    style: change overlay opacity

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
                     width: '100%',
                     height: '100%',
                     backgroundColor: 'black',
-                    opacity: 0.6,
+                    opacity: 0.5,
                     zIndex: 2
                 }}
             />
